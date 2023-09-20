607fee32033c7676ef45b49723e2206e5e4c4b57
loonglade <136028894+loonglade@users.noreply.github.com>
2023-09-20 09:50:51 -0700

    removed weather.sh

--- weather.sh
+++ weather.sh
@@ -1,2 +1,4 @@
-source venv/bin/activate
-python3 temp.py
+#!/bin/bash
+echo "Script started" >> /tmp/weather.log
+source /Users/wiggly/Documents/GitHub/Weather/venv/bin/activate
+python3 /Users/wiggly/Documents/GitHub/Weather/temp.py
