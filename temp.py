b0416917ac275f45e1a188381c591875e0aaf5fe
loonglade <136028894+loonglade@users.noreply.github.com>
2023-10-23 13:07:47 -0700

    Connection drop fix

--- temp.py
+++ temp.py
@@ -9,19 +9,29 @@ class TemperatureApp(rumps.App):
     @rumps.timer(5)
     def update_temperature(self, _):
         if self.temperature is not None:
-            self.title = f"{self.temperature}°"
+            self.title = f"{self.temperature}°C"
+        else:
+            self.title = "Loading..."
 
     def fetch_temperature(self):
-        try:
-            response = requests.get('http://192.168.1.103:8000/') # Change to whatever IP:Port you've set your board to
-            temperature = response.content.decode().split('Temperature: ')[1].split(' °')[0]
-            self.temperature = temperature
-        except Exception as e:
-            print(f"Error fetching temperature: {e}")
-            # Sleep for 5 seconds before retrying
-            time.sleep(5)
-        # Schedule the next fetch in 60 seconds
-        threading.Timer(5, self.fetch_temperature).start()
+        while True:
+            try:
+                response = requests.get('http://192.168.1.103:8000/', timeout=5)
+                if response.status_code == 200:
+                    temperature = response.content.decode().split('Temperature: ')[1].split(' °')[0]
+                    self.temperature = temperature
+                    print(f"Temperature fetched: {self.temperature}°C")
+                else:
+                    print(f"Failed to fetch temperature. Status code: {response.status_code}")
+                time.sleep(5)
+            except requests.ConnectionError:
+                print("Could not connect to the device. Retrying in 5 seconds.")
+                self.temperature = None
+                time.sleep(5)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                self.temperature = None
+                time.sleep(5)
 
 
 if __name__ == '__main__':
